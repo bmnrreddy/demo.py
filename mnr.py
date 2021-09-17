acb3984c156f86bbf09a9c1f18c5a79c7b0434e3
Optiplex 3020 User <bmrreddy9@gmail.com>
2021-09-17 13:03:30 -0400

    one line added

--- mnr.py
+++ mnr.py
@@ -1,2 +1,8 @@
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="bmnr@2429")
+
 print("malathi reddy")
 print("malathinarender reddy")
+print("malathi reddy")
+print("malathinarender reddy")
+
